9ad4594c83856709af4611710827a74b6cc46af0
Julien Vanharen <julien.vanharen@gmail.com>
2022-08-21 21:32:13 +0200

    Initial commit

--- Casks/vizir4.rb
+++ Casks/vizir4.rb
@@ -1,8 +1,8 @@
 cask "vizir4" do
-    version "2020.09.09"
-    sha256 "676d64fc9fa7dfedefb70268e4142ba926d33297a4d89c29c19cb93bee4a484a"
+    version "2022.06.09"
+    sha256 "7f247283eb1b635918e61416090804277baf8ea76d9ddf87077360c3029416a8"
 
-    url "https://pyamg.saclay.inria.fr/download/vizir/exes/Mac/vizir4.2020.09.09.dmg"
+    url "https://pyamg.saclay.inria.fr/download/vizir/exes/Mac/vizir4.2022.06.09.dmg"
     name "ViZiR 4"
     desc "ViZiR 4 is an interactive meshes and solutions visualization tool developed by the GAMMA team at Inria."
     homepage "https://pyamg.saclay.inria.fr/vizir4.html"
