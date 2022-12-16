5d06a85ca9ecd98bcebc44cbc223051089609155
Julien Vanharen <julien.vanharen@inria.fr>
2022-12-16 17:46:08 +0100

    Last version

--- Casks/vizir4.rb
+++ Casks/vizir4.rb
@@ -4,17 +4,16 @@ cask "vizir4" do
 
   url "https://pyamg.saclay.inria.fr/download/vizir/exes/Mac/vizir4.2022.06.09.dmg"
   name "ViZiR 4"
-  desc "An interactive meshes and solutions visualization tool"
+  desc "Interactive meshes and solutions visualization tool"
   homepage "https://pyamg.saclay.inria.fr/vizir4.html"
-  
+
   livecheck do
     url "https://pyamg.saclay.inria.fr/download/vizir/exes/Mac"
     strategy :page_match
     regex(/href=.*?vizir4.?(\d+(?:\.\d+)+).dmg/i)
   end
-  
-  app "vizir4.app"
 
+  app "vizir4.app"
   caveats do
     path_environment_variable "#{appdir}/vizir4.app/Contents/MacOS"
   end
