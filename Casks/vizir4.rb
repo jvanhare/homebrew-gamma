ecb0f24fa3445d058040e725cb546050886d899a
Julien Vanharen <julien.vanharen@inria.fr>
2022-12-16 17:40:57 +0100

    Last version

--- Casks/vizir4.rb
+++ Casks/vizir4.rb
@@ -1,21 +1,21 @@
 cask "vizir4" do
-    version "2022.06.09"
-    sha256 :no_check
+  version "2022.06.09"
+  sha256 :no_check
 
-    url "https://pyamg.saclay.inria.fr/download/vizir/exes/Mac/vizir4.2022.06.09.dmg"
-    name "ViZiR 4"
-    desc "ViZiR 4 is an interactive meshes and solutions visualization tool developed by the GAMMA team at Inria."
-    homepage "https://pyamg.saclay.inria.fr/vizir4.html"
+  url "https://pyamg.saclay.inria.fr/download/vizir/exes/Mac/vizir4.2022.06.09.dmg"
+  name "ViZiR 4"
+  desc "ViZiR 4 is an interactive meshes and solutions visualization tool developed by the GAMMA team at Inria."
+  homepage "https://pyamg.saclay.inria.fr/vizir4.html"
 
-    app "vizir4.app"
+  app "vizir4.app"
 
-    livecheck do
-        url "https://pyamg.saclay.inria.fr/download/vizir/exes/Mac"
-        strategy :page_match
-        regex(/href=.*?vizir4.?(\d+(?:\.\d+)+).dmg/i)
-    end
+  livecheck do
+    url "https://pyamg.saclay.inria.fr/download/vizir/exes/Mac"
+    strategy :page_match
+    regex(/href=.*?vizir4.?(\d+(?:\.\d+)+).dmg/i)
+  end
    
-    caveats do
-        path_environment_variable "#{appdir}/vizir4.app/Contents/MacOS"
-    end
+  caveats do
+    path_environment_variable "#{appdir}/vizir4.app/Contents/MacOS"
+  end
 end
