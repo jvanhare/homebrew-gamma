2a555d9e35a9e3eb2bb4eb2caa0cb903d867ef3a
Julien Vanharen <julien.vanharen@inria.fr>
2025-09-10 08:54:27 +0200

    Add tetgen

--- Formula/tetgen.rb
+++ Formula/tetgen.rb
@@ -2,7 +2,7 @@ class Tetgen < Formula
     desc "Quality Tetrahedral Mesh Generator and 3D Delaunay Triangulator"
     homepage "https://wias-berlin.de/software/tetgen"
     url "https://wias-berlin.de/software/tetgen/1.5/src/tetgen1.6.0.tar.gz"
-    sha256 "0fff0l6i3xfjlm0zkcgyyhwndp8i5d615mydyb21yirsplgfddc7"
+    sha256 "87b5e61ebd3a471fc4f2cdd7124c2b11dd6639f4feb1f941a5d2f5110d05ce39"
     license "AGPL-3.0-only"
 
     def install
