2613bdf4088eb7a984ba33eb1a3b67804adac619
Julien Vanharen <julien.vanharen@inria.fr>
2025-09-17 11:04:44 +0200

    Update libmeshb

--- Formula/libmeshb.rb
+++ Formula/libmeshb.rb
@@ -4,6 +4,7 @@ class Libmeshb < Formula
   url "https://github.com/LoicMarechal/libMeshb/archive/418213e.tar.gz"
   sha256 "db9d3091ecb7fb3478c26f0ca51535cb9f0bbac4beed7f2c9fdb539880f4a957"
   license "MIT"
+  version "7.80"
 
   depends_on "cmake" => :build
 
