bac86d92aba2f25bf333ff8368a14e673c66856f
Julien Vanharen <julien.vanharen@inria.fr>
2025-10-02 16:20:55 +0200

    libmeshb

--- Formula/libmeshb.rb
+++ Formula/libmeshb.rb
@@ -6,12 +6,10 @@ class Libmeshb < Formula
   license "MIT"
   version "7.80"
 
-  patch :DATA
-
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_BUILD_TYPE=Release", "-DCMAKE_INSTALL_PREFIX=#{prefix}"
+    system "cmake", "-S", ".", "-B", "build", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
@@ -20,20 +18,3 @@ class Libmeshb < Formula
     system "true"
   end
 end
-
-
-__END__
-diff --git a/sources/CMakeLists.txt b/sources/CMakeLists.txt
---- a/sources/CMakeLists.txt
-+++ b/sources/CMakeLists.txt
-@@
--if(CMAKE_Fortran_COMPILER)
--  install(DIRECTORY ${CMAKE_Fortran_MODULE_DIRECTORY}/ DESTINATION include)
--endif()
-+if(CMAKE_Fortran_COMPILER)
-+  if(EXISTS ${CMAKE_Fortran_MODULE_DIRECTORY})
-+    install(DIRECTORY ${CMAKE_Fortran_MODULE_DIRECTORY}/
-+            DESTINATION include
-+            FILES_MATCHING PATTERN "*.mod")
-+  endif()
-+endif()
