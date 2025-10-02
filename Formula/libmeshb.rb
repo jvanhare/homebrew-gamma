d7af4015a9bda24608d5bf2e875e4f0534b67f9c
Julien Vanharen <julien.vanharen@inria.fr>
2025-10-02 15:56:58 +0200

    libmeshb

--- Formula/libmeshb.rb
+++ Formula/libmeshb.rb
@@ -9,7 +9,7 @@ class Libmeshb < Formula
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-S", ".", "-B", "build", "-DBUILD_SHARED_LIBS=ON", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-DBUILD_SHARED_LIBS=ON", "-DCMAKE_INSTALL_INCLUDEDIR=include/libmeshb", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
