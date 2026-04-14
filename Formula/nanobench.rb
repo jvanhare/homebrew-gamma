1b60f96f79fd5ecec6c2fdbe27d121c9c0f0fbe4
Julien Vanharen <julien.vanharen@inria.fr>
2026-04-14 10:42:30 +0200

    Add nanobench

--- Formula/nanobench.rb
+++ Formula/nanobench.rb
@@ -2,7 +2,7 @@ class Nanobench < Formula
   desc "Simple, fast, accurate single-header microbenchmarking functionality for C++11/14/17/20"
   homepage "https://github.com/martinus/nanobench"
   url "https://raw.githubusercontent.com/martinus/nanobench/v4.3.11/src/include/nanobench.h"
-  sha256 "PUT_THE_CORRECT_SHA256_HERE"
+  sha256 "7d20cc0daed3f56341ccd9b5c4b1b25b2f064d76638a5ba0304a5795a32cc1b6"
   license "MIT"
 
   def install
