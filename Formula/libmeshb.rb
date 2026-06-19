c11b5a669479ad950c3a13556d2467e1b7b3b55d
Julien Vanharen <julien.vanharen@inria.fr>
2026-06-19 14:04:27 +0200

    libmeshb

--- Formula/libmeshb.rb
+++ Formula/libmeshb.rb
@@ -2,7 +2,7 @@ class Libmeshb < Formula
   desc "A library to handle the *.meshb file format."
   homepage "https://github.com/LoicMarechal/libMeshb"
   url "https://github.com/LoicMarechal/libMeshb/archive/906a83e61a2b150b85c7436acb151879fd63cfe2.tar.gz"
-  sha256 "4df46c8c0c8af333f2676615b339e856ce9bc4cd771f6eb9298657001ed6739b"
+  sha256 "6febf8c2518bd5844ca539531a64004cd17befa716665bcfe7c2ddc77f070702"
   license "MIT"
   version "8.00"
 
